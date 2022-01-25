511669662e7fc1342e1cdae0990cf69193bcf09f
Edu viade <edu.viade@alt120.com>
2022-01-25 13:12:01 +0100

    index

--- views/site/index.php
+++ views/site/index.php
@@ -7,53 +7,44 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h5 class="display-4">Congratulations!</h5>
+        <h5 class="display-4">Rutina</h5>
 
     </div>
 
     <div class="body-content">
 
         <div class="row">
+            
             <div class="col-lg-3">
                 <h5>Lunes</h5>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. </p>
-
-              
+                <p>Correr</p>
             </div>
+
             <div class="col-lg-3">
-                <h5>Heading</h5>
+                <h5>Martes</h5>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. </p>
-
-                
+                <p>Pecho, Espalda, hombros</p>
             </div>
+
             <div class="col-lg-3">
-                <h5>Heading</h5>
+                <h5>Miercoles</h5>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. </p>
-
-           
+                <p>Correr</p>
             </div>
+
             <div class="col-lg-3">
-                <h5>Heading</h5>
+                <h5>Jueves</h5>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua.</p>
-
-           
+                <p>Biceps, triceps</p>
             </div>
+
             <div class="col-lg-3">
-                <h5>Heading</h5>
+                <h5>Viernes</h5>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua.</p>
-
-           
+                <p>Pecho, espalda, hombros</p>
             </div>
+
         </div>
 
     </div>
