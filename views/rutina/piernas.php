1e65193afd762a15a10cb83980cc4259a5930172
Edu viade <edu.viade@alt120.com>
2023-06-22 17:10:25 +0200

    correciones

--- views/rutina/piernas.php
+++ views/rutina/piernas.php
@@ -9,5 +9,12 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="rutina-piernas">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <iframe width="350" height="250" src="https://www.youtube.com/embed/1rf2P4V1oKY?clip=Ugkxo_5rlOR0uXf0c_GfMu6OB5LqYNQeY_CC&amp;clipt=EJ2RExiluBM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+    <iframe width="350" height="250" src="https://www.youtube.com/embed/1rf2P4V1oKY?clip=UgkxBGK-IBa0bCA5it_MbOqUHWwoONywUNy8&amp;clipt=ENS5BBjc4AQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+    <iframe width="350" height="250" src="https://www.youtube.com/embed/1rf2P4V1oKY?clip=Ugkxav0dtVyhAaZxo-d7wFOWl5R6hhLlOt9c&amp;clipt=EIzEFRiU6xU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+    <iframe width="350" height="250" src="https://www.youtube.com/embed/1rf2P4V1oKY?clip=UgkxNOQ8cwufUoQe65B-RPee4lLYgIxJq91D&amp;clipt=EKDYHxio_x8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 </div>
