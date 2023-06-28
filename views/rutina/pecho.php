52be26a78bffea74becb39187fa75085571f0966
Edu viade <edu.viade@alt120.com>
2023-06-28 13:55:59 +0200

    new rutine

--- views/rutina/pecho.php
+++ views/rutina/pecho.php
@@ -9,18 +9,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="rutina-pecho">
     <h1><?= Html::encode($this->title) ?><img src="../upload/mas.png" width="25" height="25"></h1>
 
-
-<!-- 	<img src="../upload/pecho.gif" width="350" height="200">
-	<img src="../upload/pecho2.gif" width="350" height="200">
-	<img src="../upload/pecho3.gif" width="350" height="200">
-	<img src="../upload/pecho4.gif" width="350" height="200">
-	<img src="../upload/pecho6.gif" width="350" height="200">
-	<img src="../upload/pecho7gif.gif" width="350" height="200"> -->
-
-
-
-	<!-- <iframe width="350" height="250" src="https://www.youtube.com/embed/w86EalEoFRY?clip=UgkxaWyJq6GI60YLYdMKU8jGHsgDdWb_8y-6&amp;clipt=ENqyBxji2Qc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
-
 	<iframe width="350" height="250" src="https://www.youtube.com/embed/WDNQk_vJ2xE?clip=UgkxlvOiIbBKwz7xqFzxO-ABs2ezVf0sKe12&amp;clipt=EIzFEhjK2RM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 	<iframe width="350" height="250" src="https://www.youtube.com/embed/huB9VTclj_g?clip=UgkxkNxouojRRHLnuUA_xMsGciU5geFbjbCg&amp;clipt=ELX3CBi9ngk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -29,6 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
 	<iframe width="350" height="250" src="https://www.youtube.com/embed/bL_NIMh52E0?clip=UgkxvdtBe5Dopf8uJqJqpZhCM29PTX2hf9gk&amp;clipt=EOKuGhjq1Ro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-
+	<!-- <iframe width="350" height="250" src="https://www.youtube.com/embed/w86EalEoFRY?clip=UgkxaWyJq6GI60YLYdMKU8jGHsgDdWb_8y-6&amp;clipt=ENqyBxji2Qc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
 
 </div>
